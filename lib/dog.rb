a82cb69cc2e51c4452fdb139dc7fb6af40cd9430
madeline57 <maddiejobeth@aol.com>
2018-07-26 20:59:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog 
+  def initialize(dogs_name)
+    @this_dogs_name
+  end
+  def dogs_name
+  
+  
+  
+  
+  def name= (dog_name)
+      this_dogs_name = dog_name
+    end
+        def name 
+        this_dogs_name
+        end 
+end 
+
+
+lassie = Dog.new 
+lassie.name = "lassie"
